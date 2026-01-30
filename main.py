0727eb22aed88d258d45a05ef0954ad5b9ac0065
nbayramov551-afk <nbayramov551@gmail.com>
2026-01-31 00:12:22 +0400

    Switch to WebView for content display
    Refactor app to use WebView instead of BoxLayout for displaying content.

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 from kivy.app import App
-from kivy.uix.vbox import BoxLayout
-from android.runnable import run_on_ui_thread
+from kivy.uix.widget import Widget
 from jnius import autoclass
+from android.runnable import run_on_ui_thread
 
+# Android sistem kitabxanaları
 WebView = autoclass('android.webkit.WebView')
 WebViewClient = autoclass('android.webkit.WebViewClient')
 Activity = autoclass('org.kivy.android.PythonActivity').mActivity
 
 class TitaniumApp(App):
     def build(self):
-        self.layout = BoxLayout(orientation='vertical')
-        self.open_link()
-        return self.layout
+        # Boş bir widget yaradırıq, çünki biz birbaşa Android ekranını istifadə edəcəyik
+        self.open_webview()
+        return Widget()
 
     @run_on_ui_thread
-    def open_link(self):
+    def open_webview(self):
         webview = WebView(Activity)
         settings = webview.getSettings()
+        
+        # Ən vacib ayarlar (Localhost üçün)
         settings.setJavaScriptEnabled(True)
         settings.setDomStorageEnabled(True)
+        settings.setAllowFileAccess(True)
+        
         webview.setWebViewClient(WebViewClient())
-        # ÖZ REAL LİNKİNİ BURAYA YAZ
-        webview.loadUrl("https://sənin-sayt-linkin.com")
+        
+        # BURAYA DİQQƏT: Localhost üçün Termux IP-ni bura qoymalısan
+        # Məsələn: webview.loadUrl("http://192.168.1.5:8080")
+        webview.loadUrl("https://google.com") # İlk yoxlama üçün
+        
         Activity.setContentView(webview)
 
 if __name__ == '__main__':
