bf8cd4c3ec18073f0c6be19e6bbc99e513de7f29
nbayramov551-afk <nbayramov551@gmail.com>
2026-01-30 23:08:15 +0400

    Rename class and update webview initialization

--- main.py
+++ main.py
@@ -3,34 +3,26 @@ from kivy.uix.vbox import BoxLayout
 from android.runnable import run_on_ui_thread
 from jnius import autoclass
 
-# Android-in daxili komponentləri
 WebView = autoclass('android.webkit.WebView')
 WebViewClient = autoclass('android.webkit.WebViewClient')
 Activity = autoclass('org.kivy.android.PythonActivity').mActivity
 
-class TitaniumShell(App):
+class TitaniumApp(App):
     def build(self):
         self.layout = BoxLayout(orientation='vertical')
-        self.create_webview()
+        self.open_link()
         return self.layout
 
     @run_on_ui_thread
-    def create_webview(self):
+    def open_link(self):
         webview = WebView(Activity)
-        settings = webview.getSettings()
-        settings.setJavaScriptEnabled(True)
-        settings.setDomStorageEnabled(True)
-        settings.setDatabaseEnabled(True)
-        settings.setAllowFileAccess(True)
-        settings.setMixedContentMode(0)
-        
+        webview.getSettings().setJavaScriptEnabled(True)
+        webview.getSettings().setDomStorageEnabled(True)
         webview.setWebViewClient(WebViewClient())
-        
-        # 🔗 LİNKİNİ BURAYA QOY
-        webview.loadUrl("SƏNİN_CANLI_LİNKİN") 
-        
+        # ÖZ LİNKİNİ BURA QOY:
+        webview.loadUrl("https://ismayil-titanium-link.com")
         Activity.setContentView(webview)
 
 if __name__ == '__main__':
-    TitaniumShell().run()
-  
+    TitaniumApp().run()
+    
